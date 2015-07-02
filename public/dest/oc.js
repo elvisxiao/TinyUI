29daf9c115de8b9d5bae4cb4c3e875024b033ae1
Elvis <elvis.xiao@oceanwing.com>
2015-07-02 17:53:31 +0800

    save

--- public/dest/oc.js
+++ public/dest/oc.js
@@ -1358,115 +1358,12 @@ module.exports = UI;
 },{}],12:[function(require,module,exports){
 var ZDate = require('./date');
 
-var instance = function(ele){
+var instance = function(ele, theme){
 	var ele = $(ele);
-	ele.on('click', function(){
-		_setPopEle(ele);
+	ele.on('click', function(e){
+		e.stopPropagation();
+		new Weekpicker(this, theme);
 	})
-    
-    ele.on('blur', function(){
-        _removePopEle(this);
-    })
-}
-
-var _removePopEle = function(target){
-	// var ele = $(target);
-	// ele.data('zTarget') && ele.data('zTarget').remove && ele.data('zTarget').remove();
-	// ele.data('zTarget', null);
-}
-
-var _setPopEle = function(target){
-	var ele = $(target);
-
-	var elePop = $('<div class="zWeekpickerWrapBottom"><div class="zWeekpickerHd"><i class="iPrev">&lt;</i><span class="zWeekpickerHdContent">2015</span><i class="iNext">&gt;</i></div><div class="zWeekpickerBd"></div></div>');
-	
-	_showViewWeek(null, elePop.find('.zWeekpickerBd'));
-	elePop.appendTo('body');
-	var position = ele.offset();
-	var left = position.left;
-	var top = position.top;
-	top = ele.offset().top + ele.outerHeight() + 8;
-	if(top + elePop.height() > $(document).height()){
-		elePop.removeClass('zWeekpickerWrapBottom').addClass('zWeekpickerWrap');
-		top = top - elePop.outerHeight() - 8;
-	}
-	if(left + elePop.outerWidth() > $(document).scrollLeft() + $('body').width()){
-		left = left - elePop.outerWidth() + ele.outerWidth();
-		elePop.removeClass('zWeekpickerWrap').addClass('zWeekpickerWrapRight');
-	}
-	elePop.css({
-		left: left,
-		top: top
-	})
-	.on('click', '.zWeekpickerHdContent', function(){
-    	_showViewYear(this.innerHTML, this.parentNode);
-    })
-    .on('click', '.zWeekpickerBd>span', function(){
-    	var weekpickerBody = $(this).parent();
-    	if(weekpickerBody.hasClass('zWeekViewYear')){
-    		_showViewWeek(this.innerHTML, this.parentNode);
-    	}
-    }).
-    on('click', '.zWeekpickerHd>i', function(){
-    	var i = $(this);
-    	var hd = i.parent();
-    	var hdContentEle = hd.find('.zWeekpickerHdContent');
-    	var year = hdContentEle.html();
-    	if(year.length === 4){
-    		year = parseInt(year);
-    		i.hasClass('.iPrev')? year -- : year ++;
-    		hdContentEle.html(year);
-    	}
-    	else{
-    		year = parseInt(year.slice(0, 4)) + 5;
-    		i.hasClass('.iPrev')? (year -= 10) : (year += 10);
-    		
-    	}
-    });
-
-	ele.data('zTarget', elePop);
-}
-
-var _showViewWeek = function(year, bd){
-	bd = $(bd);
-	var content = '';
-	var currWeek = ZDate.getWeekString(new Date()).toString();
-
-	if(!year){
-		year = currWeek.slice(0, 4);
-	}
-
-	bd.prev('.zWeekpickerHd').find('.zWeekpickerHdContent').html(year);
-	for(var i = 1; i < 53; i++){
-		var val = i.toString();
-		if(val < 10){
-			val = '0' + val;
-		}
-		content += '<span>' + val + '</span>';
-	}
-	bd.html(content).removeClass('zWeekViewYear');
-	
-	bd.find('>span:contains(' + currWeek.slice(4) + ')').addClass('active');
-}
-
-var _showViewYear = function(year, hd){
-	if(year.length > 4){
-		return;
-	}
-
-	hd = $(hd);
-	year = parseInt(year);
-	var spanYear = (year - 5) +  ' - ' + (year + 5);
-	hd.find('.zWeekpickerHdContent').html(spanYear);
-
-	var content = '';
-	for(var i = year -6; i < year + 6; i++){
-		content += '<span>' + i + '</span>';
-	}
-
-	var bd = hd.next('.zWeekpickerBd').html(content).addClass('zWeekViewYear');
-	bd.find('>span:first-child, >span:last-child').addClass('weekOld');
-	bd.find('>span:contains(' + year + ')').addClass('active');
 }
 
 var initEvent = function(){
@@ -1474,25 +1371,180 @@ var initEvent = function(){
         $('body')
         .off('click', '.zWeekpicker')
         .on('click', '.zWeekpicker', function(e){
-            _setPopEle(this);
+            new Weekpicker(this, this.getAttribute('data-theme'));
+            e.stopPropagation();
         })
-        // .off('blur', 'zWeekpicker')
-        // .on('blur', '.zWeekpicker', function(){
-        //     _removePopEle(this);
-        // })
-        // .on('click', '.zWeekpickerHdContent', function(){
-        // 	_showViewYear(this.innerHTML, this.parentNode);
-        // })
-        // .on('click', '.zWeekpickerBd>span', function(){
-        // 	var weekpickerBody = $(this).parent();
-        // 	if(weekpickerBody.hasClass('zWeekViewYear')){
-        // 		_showViewWeek(this.innerHTML, this.parentNode);
-        // 	}
-        // })
+        .on('click', function(){
+        	$('.zWeekpickerHd').parent().hide()
+        })
     })  
 }
-
 initEvent();
+
+var Weekpicker = function(target, theme) {
+	this.hd = null;
+	this.bd = null;
+	this.view; //week or year
+	this.target;
+	this.ele;
+	this.year; // 当前的年
+	this.weekString; //初始的weekString
+
+	var self = this;
+
+	self.render = function(){
+		self.view === 'week'? self._renderWeek() : self._renderYear();
+	}
+
+	self.setPosition = function(){
+		var position = self.target.offset();
+		var left = position.left;
+		var top = position.top;
+
+		var eleTop = top + self.target.outerHeight() + 8;
+		var win = $(window);
+
+		self.ele.addClass('zWeekpickerWrapBottom').removeClass('zWeekpickerWrap').removeClass('zWeekpickerWrapRight');
+		if( eleTop + self.ele.height() > win.outerHeight() + win.scrollTop() ){
+			self.ele.removeClass('zWeekpickerWrapBottom').addClass('zWeekpickerWrap');
+			eleTop = top - self.ele.outerHeight() - 8;
+		}
+		if( left + self.ele.outerWidth() > win.scrollLeft() + win.width() ){
+			left = left - self.ele.outerWidth() + self.target.outerWidth();
+			self.ele.removeClass('zWeekpickerWrap').addClass('zWeekpickerWrapRight');
+		}
+		this.ele.css({
+			left: left,
+			top: eleTop
+		})
+	}
+
+	self._renderWeek = function(){
+		this.hd.find('.zWeekpickerHdContent').html(this.year);
+		var weekCount = ZDate.getWeeksByYear(this.year);
+		var text = '';
+		for(var i = 1; i <= weekCount; i ++){
+			var strWeek = i;
+			i < 10 && ( strWeek = '0' + i.toString() );
+			text += '<span>' + strWeek + '</span>';
+		}
+		this.bd.html(text).removeClass('zWeekViewYear');;
+		if(this.weekString.indexOf( this.year.toString() ) === 0){
+			this.bd.find('>span:contains(' + this.weekString.slice(4) + ')').addClass('active');
+		}
+		self._setTimeInfo(this.weekString);
+
+		this.view = 'week';
+	}
+
+	self._setTimeInfo = function(weekStr){
+		var startDate = ZDate.getStartDateByWeek(weekStr).getTime();
+		var endDate = startDate + 6 * 24 * 60 * 60000;
+		var label = self.bd.find('>.timeInfo');
+		if(label.length === 0){
+			label = $('<label class="timeInfo"></label>').appendTo(self.bd);
+		}
+		label.html('<i class="icon-clock2"></i>' + ZDate.format(startDate, 'mmdd') + ' - ' + ZDate.format(endDate, 'mmdd'));
+	}
+
+	self._renderYear = function(){
+		var year = parseInt(this.year);
+		var startYear = parseInt(year / 10) * 10;
+
+		this.hd.find('.zWeekpickerHdContent').html( startYear +  ' - ' + (startYear + 9) );
+			
+		var text = '';
+		for(var i = 0; i < 9; i++){
+			text += '<span>' + (startYear + i) + '</span>';
+		}
+
+		this.bd.html(text).addClass('zWeekViewYear');
+
+		this.bd.find('>span:first-child, >span:last-child').addClass('weekOld');
+		
+		this.bd.find('>span:contains(' + self.weekString.slice(0, 4) + ')').addClass('active');
+
+		this.view = 'year';
+	}
+
+	self._bindEvents = function(){
+		self.ele.off()
+		.on('click', function(e){
+	    	e.stopPropagation();
+		})
+		.on('click', '.zWeekpickerHdContent', function(){
+	    	self.view === 'week' && self._renderYear();
+	    })
+	    .on('click', '.zWeekpickerBd>span', function(){
+	    	if(self.view === 'year'){
+	    		self.year = this.innerHTML;
+	    		self._renderWeek();
+	    	}
+	    	else{
+	    		self.target.val(self.year + this.innerHTML).change();
+	    		self.ele.hide();
+	    	}
+	    })
+	    .on('click', '.zWeekpickerHd>i', function(e){
+	    	var i = $(this);
+	    	var hdContent = self.hd.find('.zWeekpickerHdContent');
+	    	var year = hdContent.text();
+
+	    	if(self.view === 'week'){
+	    		year = parseInt(year);
+	    		i.hasClass('iPrev')? year -- : year ++;
+	    		self.year = year;
+	    		self._renderWeek();
+	    	}
+	    	else{
+	    		year = parseInt(year.slice(0, 4));
+	    		i.hasClass('iPrev')? (year -= 10) : (year += 10);
+	    		self.year = year + 5;
+	    		self._renderYear();
+	    	}
+	    })
+	    .on('mouseenter', '.zWeekpickerBd>span', function(){
+	    	if(self.view === 'year'){
+	    		return;
+	    	}
+
+	    	var weekString = self.year + this.innerText;
+
+	    	self._setTimeInfo(weekString);
+	    })
+	    .on('mouseout', '.zWeekpickerBd>span', function(){
+	    	if(self.view === 'year'){
+	    		return;
+	    	}
+	    	self._setTimeInfo(self.weekString);
+	    })
+	}
+
+	self.init = function(target, theme){
+		self.target = $(target);
+
+		self.ele = $('.zWeekpickerHd').parent();
+		if(self.ele.length === 0){
+			self.ele = $('<div class="zWeekpickerWrapBottom"><div class="zWeekpickerHd"><i class="iPrev">&lt;</i><span class="zWeekpickerHdContent"></span><i class="iNext">&gt;</i></div><div class="zWeekpickerBd"></div></div>').appendTo('body');
+		}
+		
+		self.ele.attr('data-theme', theme || '')
+		
+		self._bindEvents();
+		self.ele.show();
+		
+		self.hd = self.ele.find('.zWeekpickerHd');
+		self.bd = self.ele.find('.zWeekpickerBd');
+		self.view = 'week';
+		self.weekString = self.target.val() || ZDate.getWeekString(new Date()).toString();
+		self.year = self.weekString.slice(0, 4);
+		self.render();
+		self.setPosition();
+	}
+
+	self.init(target, theme);
+}
+
 
 module.exports = instance;
 
